4806dec032cb4c7995b6ef71ac50d2470dfbf1a2
bienvenu7 <ngadzieb@gmail.com>
2025-03-01 05:10:03 +0300

    done8

--- src/instance.ts
+++ src/instance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const baseURL =
   process.env.NODE_ENV === "production"
-    ? "https://transfery-api.onrender.com"
+    ? "https://transfery-api.onrender.com/v1/"
     : "http://localhost:9090/v1/";
 
 export const instance = axios.create({
