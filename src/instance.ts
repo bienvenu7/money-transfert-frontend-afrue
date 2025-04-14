333485422e6a7cf57aa6441b6afa471bf6794f57
bienvenu7 <ngadzieb@gmail.com>
2025-04-14 18:27:05 +0300

    added last commit

--- src/instance.ts
+++ src/instance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const baseURL =
   process.env.NODE_ENV === "production"
-    ? "https://https://money-transfert-api.onrender.com/v1/"
+    ? "https://money-transfert-api.onrender.com/v1/"
     : "http://localhost:9090/v1/";
 
 export const instance = axios.create({
