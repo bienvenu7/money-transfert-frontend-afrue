546250115c5da70c6290dbd46137736085cc275d
bienvenu7 <ngadzieb@gmail.com>
2025-08-06 11:51:55 +0300

    https://gitlab.gteh.pro/gteh/frontend/club-front/-/issues/19

--- src/instance.ts
+++ src/instance.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const baseURL =
   process.env.NODE_ENV === "production"
     ? "http://82.146.49.197/v1/"
-    : "http://localhost:9090/v1/";
+    : "http://localhost:7001/v1/";
 
 export const instance = axios.create({
   baseURL: baseURL,
